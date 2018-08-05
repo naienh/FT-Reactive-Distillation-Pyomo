36cab7f943cd08d68608414fa583733d5ec94e58
naienh <naienh@andrew.cmu.edu>
2018-08-05 00:49:25 -0400

    finalized sequence

--- utility/display_utility.py
+++ utility/display_utility.py
@@ -441,7 +441,6 @@ def plot_distribution(model,open_log_pdf = None,title = None):
         open_log_pdf.savefig()
 
     plt.show()
-
     plt.close(fig)
 
 def plot_product_distribution(model,open_log_pdf = None):
